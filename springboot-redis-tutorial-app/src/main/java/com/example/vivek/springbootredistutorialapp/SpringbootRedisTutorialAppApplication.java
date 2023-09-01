b715e99186717f85ec54857b821543278b5601c7
VivekDreamer <vivekmakv@gmail.com>
2023-09-01 23:05:33 +0530

    initial project structure commit

---
+++ springboot-redis-tutorial-app/src/main/java/com/example/vivek/springbootredistutorialapp/SpringbootRedisTutorialAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.vivek.springbootredistutorialapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisTutorialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisTutorialAppApplication.class, args);
+	}
+
+}
