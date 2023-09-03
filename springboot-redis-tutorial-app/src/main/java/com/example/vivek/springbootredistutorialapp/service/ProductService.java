4a4a316623871cdb4758321e93f77be920d9fc14
VivekDreamer <vivekmakv@gmail.com>
2023-09-03 17:55:54 +0000

    service class is added

---
+++ springboot-redis-tutorial-app/src/main/java/com/example/vivek/springbootredistutorialapp/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.example.vivek.springbootredistutorialapp.service;
+
+import java.util.List;
+import com.example.vivek.springbootredistutorialapp.model.Product;
+
+public interface ProductService {
+    public Product save(Product product);
+    public List<Product> findAll();
+    public Product findProductById(int id);
+    public String deleteProduct(int id);
+}
