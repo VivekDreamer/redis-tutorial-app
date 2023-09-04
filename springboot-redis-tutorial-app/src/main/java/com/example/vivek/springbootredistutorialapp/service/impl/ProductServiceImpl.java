e9abebc29f1c0866c89fe7836082a99123566a64
VivekDreamer <vivekmakv@gmail.com>
2023-09-04 16:42:53 +0000

    Spring data redis as a cache

--- springboot-redis-tutorial-app/src/main/java/com/example/vivek/springbootredistutorialapp/service/impl/ProductServiceImpl.java
+++ springboot-redis-tutorial-app/src/main/java/com/example/vivek/springbootredistutorialapp/service/impl/ProductServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -34,13 +36,20 @@ public class ProductServiceImpl implements ProductService{
     return list;
 
     }
-
+ 
     @Override
+    // if we want to cache the result obtain by the api 
+    // @Cacheable(key = "#id", value = "Product")
+    // if you want to cache the result based on the condition then, apply as below
+    //in this, if price is less than or equal to 1000, then only it will store the value in cache
+    @Cacheable(key = "#id", value = "Product", unless = "#result.price > 1000")
     public Product findProductById(int id) {
+        System.out.println("inside findProductById() ::: ");
         return (Product) redisTemplate.opsForHash().get(HASH_KEY, id);
     }
 
     @Override
+    @CacheEvict(key = "#id", value = "Product")
     public String deleteProduct(int id) {
         redisTemplate.opsForHash().delete(HASH_KEY,id);
         return "product deleted";
